ff4e314943db8d763dbae62e05cb90b03bb2bcf6
wemih <white9300@gmail.com>
2022-03-07 14:19:37 +0100

    Add project files.

---
+++ MemoryGame/Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace MemoryGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startBTN = new System.Windows.Forms.Button();
+            this.timerLBL = new System.Windows.Forms.Label();
+            this.cardPB1 = new System.Windows.Forms.PictureBox();
+            this.cardPB2 = new System.Windows.Forms.PictureBox();
+            this.cardPB3 = new System.Windows.Forms.PictureBox();
+            this.cardPB4 = new System.Windows.Forms.PictureBox();
+            this.cardPB5 = new System.Windows.Forms.PictureBox();
+            this.cardPB6 = new System.Windows.Forms.PictureBox();
+            this.cardPB7 = new System.Windows.Forms.PictureBox();
+            this.cardPB8 = new System.Windows.Forms.PictureBox();
+            this.cardPB9 = new System.Windows.Forms.PictureBox();
+            this.cardPB10 = new System.Windows.Forms.PictureBox();
+            this.cardPB11 = new System.Windows.Forms.PictureBox();
+            this.cardPB12 = new System.Windows.Forms.PictureBox();
+            this.cardPB13 = new System.Windows.Forms.PictureBox();
+            this.cardPB14 = new System.Windows.Forms.PictureBox();
+            this.cardPB15 = new System.Windows.Forms.PictureBox();
+            this.cardPB16 = new System.Windows.Forms.PictureBox();
+            this.cardPB17 = new System.Windows.Forms.PictureBox();
+            this.cardPB18 = new System.Windows.Forms.PictureBox();
+            this.cardPB19 = new System.Windows.Forms.PictureBox();
+            this.cardPB20 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB13)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB14)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB15)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB16)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB17)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB18)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB19)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB20)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startBTN
+            // 
+            this.startBTN.Location = new System.Drawing.Point(788, 25);
+            this.startBTN.Name = "startBTN";
+            this.startBTN.Size = new System.Drawing.Size(75, 23);
+            this.startBTN.TabIndex = 0;
+            this.startBTN.Text = "Játék";
+            this.startBTN.UseVisualStyleBackColor = true;
+            this.startBTN.Click += new System.EventHandler(this.starGame);
+            // 
+            // timerLBL
+            // 
+            this.timerLBL.AutoSize = true;
+            this.timerLBL.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.timerLBL.Location = new System.Drawing.Point(467, 12);
+            this.timerLBL.Name = "timerLBL";
+            this.timerLBL.Size = new System.Drawing.Size(35, 18);
+            this.timerLBL.TabIndex = 1;
+            this.timerLBL.Text = "100";
+            // 
+            // cardPB1
+            // 
+            this.cardPB1.Location = new System.Drawing.Point(12, 12);
+            this.cardPB1.Name = "cardPB1";
+            this.cardPB1.Size = new System.Drawing.Size(100, 100);
+            this.cardPB1.TabIndex = 2;
+            this.cardPB1.TabStop = false;
+            this.cardPB1.Click += new System.EventHandler(this.clickImage);
+            // 
+            // cardPB2
+            // 
+            this.cardPB2.Location = new System.Drawing.Point(118, 12);
+            this.cardPB2.Name = "cardPB2";
+            this.cardPB2.Size = new System.Drawing.Size(100, 100);
+            this.cardPB2.TabIndex = 3;
+            this.cardPB2.TabStop = false;
+            this.cardPB2.Click += new System.EventHandler(this.clickImage);
+            // 
+            // cardPB3
+            // 
+            this.cardPB3.Location = new System.Drawing.Point(224, 12);
+            this.cardPB3.Name = "cardPB3";
+            this.cardPB3.Size = new System.Drawing.Size(100, 100);
+            this.cardPB3.TabIndex = 4;
+            this.cardPB3.TabStop = false;
+            this.cardPB3.Click += new System.EventHandler(this.clickImage);
+            // 
+            // cardPB4
+            // 
+            this.cardPB4.Location = new System.Drawing.Point(330, 12);
+            this.cardPB4.Name = "cardPB4";
+            this.cardPB4.Size = new System.Drawing.Size(100, 100);
+            this.cardPB4.TabIndex = 5;
+            this.cardPB4.TabStop = false;
+            this.cardPB4.Click += new System.EventHandler(this.clickImage);
+            // 
+            // cardPB5
+            // 
+            this.cardPB5.Location = new System.Drawing.Point(12, 118);
+            this.cardPB5.Name = "cardPB5";
+            this.cardPB5.Size = new System.Drawing.Size(100, 100);
+            this.cardPB5.TabIndex = 6;
+            this.cardPB5.TabStop = false;
+            this.cardPB5.Click += new System.EventHandler(this.clickImage);
+            // 
+            // cardPB6
+            // 
+            this.cardPB6.Location = new System.Drawing.Point(118, 118);
+            this.cardPB6.Name = "cardPB6";
+            this.cardPB6.Size = new System.Drawing.Size(100, 100);
+            this.cardPB6.TabIndex = 7;
+            this.cardPB6.TabStop = false;
+            this.cardPB6.Click += new System.EventHandler(this.clickImage);
+            // 
+            // cardPB7
+            // 
+            this.cardPB7.Location = new System.Drawing.Point(224, 118);
+            this.cardPB7.Name = "cardPB7";
+            this.cardPB7.Size = new System.Drawing.Size(100, 100);
+            this.cardPB7.TabIndex = 8;
+            this.cardPB7.TabStop = false;
+            this.cardPB7.Click += new System.EventHandler(this.clickImage);
+            // 
+            // cardPB8
+            // 
+            this.cardPB8.Location = new System.Drawing.Point(330, 118);
+            this.cardPB8.Name = "cardPB8";
+            this.cardPB8.Size = new System.Drawing.Size(100, 100);
+            this.cardPB8.TabIndex = 9;
+            this.cardPB8.TabStop = false;
+            this.cardPB8.Click += new System.EventHandler(this.clickImage);
+            // 
+            // cardPB9
+            // 
+            this.cardPB9.Location = new System.Drawing.Point(12, 224);
+            this.cardPB9.Name = "cardPB9";
+            this.cardPB9.Size = new System.Drawing.Size(100, 100);
+            this.cardPB9.TabIndex = 10;
+            this.cardPB9.TabStop = false;
+            this.cardPB9.Click += new System.EventHandler(this.clickImage);
+            // 
+            // cardPB10
+            // 
+            this.cardPB10.Location = new System.Drawing.Point(118, 224);
+            this.cardPB10.Name = "cardPB10";
+            this.cardPB10.Size = new System.Drawing.Size(100, 100);
+            this.cardPB10.TabIndex = 11;
+            this.cardPB10.TabStop = false;
+            this.cardPB10.Click += new System.EventHandler(this.clickImage);
+            // 
+            // cardPB11
+            // 
+            this.cardPB11.Location = new System.Drawing.Point(224, 224);
+            this.cardPB11.Name = "cardPB11";
+            this.cardPB11.Size = new System.Drawing.Size(100, 100);
+            this.cardPB11.TabIndex = 12;
+            this.cardPB11.TabStop = false;
+            this.cardPB11.Click += new System.EventHandler(this.clickImage);
+            // 
+            // cardPB12
+            // 
+            this.cardPB12.Location = new System.Drawing.Point(330, 224);
+            this.cardPB12.Name = "cardPB12";
+            this.cardPB12.Size = new System.Drawing.Size(100, 100);
+            this.cardPB12.TabIndex = 13;
+            this.cardPB12.TabStop = false;
+            this.cardPB12.Click += new System.EventHandler(this.clickImage);
+            // 
+            // cardPB13
+            // 
+            this.cardPB13.Location = new System.Drawing.Point(12, 330);
+            this.cardPB13.Name = "cardPB13";
+            this.cardPB13.Size = new System.Drawing.Size(100, 100);
+            this.cardPB13.TabIndex = 14;
+            this.cardPB13.TabStop = false;
+            this.cardPB13.Click += new System.EventHandler(this.clickImage);
+            // 
+            // cardPB14
+            // 
+            this.cardPB14.Location = new System.Drawing.Point(118, 330);
+            this.cardPB14.Name = "cardPB14";
+            this.cardPB14.Size = new System.Drawing.Size(100, 100);
+            this.cardPB14.TabIndex = 15;
+            this.cardPB14.TabStop = false;
+            this.cardPB14.Click += new System.EventHandler(this.clickImage);
+            // 
+            // cardPB15
+            // 
+            this.cardPB15.Location = new System.Drawing.Point(224, 330);
+            this.cardPB15.Name = "cardPB15";
+            this.cardPB15.Size = new System.Drawing.Size(100, 100);
+            this.cardPB15.TabIndex = 16;
+            this.cardPB15.TabStop = false;
+            this.cardPB15.Click += new System.EventHandler(this.clickImage);
+            // 
+            // cardPB16
+            // 
+            this.cardPB16.Location = new System.Drawing.Point(330, 330);
+            this.cardPB16.Name = "cardPB16";
+            this.cardPB16.Size = new System.Drawing.Size(100, 100);
+            this.cardPB16.TabIndex = 17;
+            this.cardPB16.TabStop = false;
+            this.cardPB16.Click += new System.EventHandler(this.clickImage);
+            // 
+            // cardPB17
+            // 
+            this.cardPB17.Location = new System.Drawing.Point(12, 436);
+            this.cardPB17.Name = "cardPB17";
+            this.cardPB17.Size = new System.Drawing.Size(100, 100);
+            this.cardPB17.TabIndex = 18;
+            this.cardPB17.TabStop = false;
+            this.cardPB17.Click += new System.EventHandler(this.clickImage);
+            // 
+            // cardPB18
+            // 
+            this.cardPB18.Location = new System.Drawing.Point(118, 436);
+            this.cardPB18.Name = "cardPB18";
+            this.cardPB18.Size = new System.Drawing.Size(100, 100);
+            this.cardPB18.TabIndex = 19;
+            this.cardPB18.TabStop = false;
+            this.cardPB18.Click += new System.EventHandler(this.clickImage);
+            // 
+            // cardPB19
+            // 
+            this.cardPB19.Location = new System.Drawing.Point(224, 436);
+            this.cardPB19.Name = "cardPB19";
+            this.cardPB19.Size = new System.Drawing.Size(100, 100);
+            this.cardPB19.TabIndex = 20;
+            this.cardPB19.TabStop = false;
+            this.cardPB19.Click += new System.EventHandler(this.clickImage);
+            // 
+            // cardPB20
+            // 
+            this.cardPB20.Location = new System.Drawing.Point(330, 436);
+            this.cardPB20.Name = "cardPB20";
+            this.cardPB20.Size = new System.Drawing.Size(100, 100);
+            this.cardPB20.TabIndex = 21;
+            this.cardPB20.TabStop = false;
+            this.cardPB20.Click += new System.EventHandler(this.clickImage);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(897, 572);
+            this.Controls.Add(this.cardPB20);
+            this.Controls.Add(this.cardPB19);
+            this.Controls.Add(this.cardPB18);
+            this.Controls.Add(this.cardPB17);
+            this.Controls.Add(this.cardPB16);
+            this.Controls.Add(this.cardPB15);
+            this.Controls.Add(this.cardPB14);
+            this.Controls.Add(this.cardPB13);
+            this.Controls.Add(this.cardPB12);
+            this.Controls.Add(this.cardPB11);
+            this.Controls.Add(this.cardPB10);
+            this.Controls.Add(this.cardPB9);
+            this.Controls.Add(this.cardPB8);
+            this.Controls.Add(this.cardPB7);
+            this.Controls.Add(this.cardPB6);
+            this.Controls.Add(this.cardPB5);
+            this.Controls.Add(this.cardPB4);
+            this.Controls.Add(this.cardPB3);
+            this.Controls.Add(this.cardPB2);
+            this.Controls.Add(this.cardPB1);
+            this.Controls.Add(this.timerLBL);
+            this.Controls.Add(this.startBTN);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB13)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB14)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB15)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB16)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB17)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB18)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB19)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB20)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startBTN;
+        private System.Windows.Forms.Label timerLBL;
+        private System.Windows.Forms.PictureBox cardPB1;
+        private System.Windows.Forms.PictureBox cardPB2;
+        private System.Windows.Forms.PictureBox cardPB3;
+        private System.Windows.Forms.PictureBox cardPB4;
+        private System.Windows.Forms.PictureBox cardPB5;
+        private System.Windows.Forms.PictureBox cardPB6;
+        private System.Windows.Forms.PictureBox cardPB7;
+        private System.Windows.Forms.PictureBox cardPB8;
+        private System.Windows.Forms.PictureBox cardPB9;
+        private System.Windows.Forms.PictureBox cardPB10;
+        private System.Windows.Forms.PictureBox cardPB11;
+        private System.Windows.Forms.PictureBox cardPB12;
+        private System.Windows.Forms.PictureBox cardPB13;
+        private System.Windows.Forms.PictureBox cardPB14;
+        private System.Windows.Forms.PictureBox cardPB15;
+        private System.Windows.Forms.PictureBox cardPB16;
+        private System.Windows.Forms.PictureBox cardPB17;
+        private System.Windows.Forms.PictureBox cardPB18;
+        private System.Windows.Forms.PictureBox cardPB19;
+        private System.Windows.Forms.PictureBox cardPB20;
+    }
+}
+
---
+++ MemoryGame/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MemoryGame
+{
+    //Create global variables
+    public partial class Form1 : Form
+    {
+        bool allowClick = false;
+        PictureBox firstGuess;
+        Random rnd = new Random();
+        Timer clickTimer = new Timer();
+        int time = 100; //countdown timer
+        Timer timer = new Timer { Interval = 1000 }; //1000ms = 1 sec
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        //Creating an Array with the pictureBoxes
+        private PictureBox[] pictureBoxes
+        {
+            get { return Controls.OfType<PictureBox>().ToArray(); }
+        }
+
+        //addig images to the pictureBoxes
+        private static IEnumerable<Image> images
+        {
+            get
+            {
+                return new Image[]
+                {
+                    Properties.Resources._1,
+                    Properties.Resources._2,
+                    Properties.Resources._3,
+                    Properties.Resources._4,
+                    Properties.Resources._5,
+                    Properties.Resources._6,
+                    Properties.Resources._7,
+                    Properties.Resources._8,
+                    Properties.Resources._9,
+                    Properties.Resources._10,
+                };
+            }
+        }
+
+        //Flipping images down to show a question mark
+        private void hideImages()
+        {
+            foreach (var pic in pictureBoxes)
+            {
+                pic.Image = Properties.Resources._11;
+            }
+        }
+
+        //Check empty pictureboxes
+        private PictureBox getFreeSlot()
+        {
+            int num;
+
+            do
+            {
+                num = rnd.Next(0, pictureBoxes.Count());
+            }
+            while (pictureBoxes[num].Tag != null);
+            return pictureBoxes[num];
+        }
+
+        //Find pairs of images
+        private void setRndImgs()
+        {
+            foreach (var image in images)
+            {
+                getFreeSlot().Tag = image;
+                getFreeSlot().Tag = image;
+            }
+        }
+
+        //For resetting the pictureBoxes
+        private void ResetImages()
+        {
+            foreach (var pic in pictureBoxes)
+            {
+                pic.Tag = null;
+                pic.Visible = true;
+            }
+
+            hideImages();
+            setRndImgs();
+            time = 100;
+            timer.Start();
+        }
+
+        //To start and display the timer
+        private void startGameTimer()
+        {
+            timer.Start();
+            timer.Tick += delegate
+            {
+                time--;
+                if (time < 0)
+                {
+                    timer.Stop();
+                    MessageBox.Show("Lejárt az idő!");
+                    ResetImages();
+                }
+                var ssTime = TimeSpan.FromSeconds(time);
+
+                timerLBL.Text = " " + time.ToString();
+            };
+        }
+
+        //hide images and stop timer
+        private void clickTimerTick(object sender, EventArgs e)
+        {
+            hideImages();
+
+            allowClick = true;
+            clickTimer.Stop();
+
+        }
+
+        //Image clicking settings
+        private void clickImage(object sender, EventArgs e)
+        {
+            if (!allowClick) return;
+
+            var pic = (PictureBox)sender;
+
+            //Check if we are able to make our first guess
+            if (firstGuess == null)
+            {
+                firstGuess = pic;
+                pic.Image = (Image)pic.Tag;
+                return;
+            }
+
+            //If the image was found set the tag
+            pic.Image = (Image)pic.Tag;
+
+            if(pic.Image == firstGuess.Image && pic != firstGuess)
+
+            {
+                pic.Visible = firstGuess.Visible = false;
+                {
+                    firstGuess = pic;
+                }
+                hideImages();
+            }
+            else
+            {
+                allowClick = false;
+                clickTimer.Start();
+            }
+
+            firstGuess = null;
+
+            //Check if  there are any visible picturebox
+            if (pictureBoxes.Any(p => p.Visible)) return;
+
+            //If all matched you won
+            MessageBox.Show("Gratulálok nyertél!");
+            ResetImages();
+        }
+
+        //stargameButton
+        private void starGame(object sender, EventArgs e)
+        {
+            allowClick = true;
+            setRndImgs();
+            hideImages();
+            startGameTimer();
+            clickTimer.Interval = 1000;
+            clickTimer.Tick += clickTimerTick;
+            startBTN.Enabled = false;
+
+        }
+    }
+}
